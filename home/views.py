c0d92494b011ea4c51052b39fbce441fdda3807d
9730991252 <sunilkale101@gmail.com>
2024-11-24 16:45:29 +0530

    ok

--- home/views.py
+++ home/views.py
@@ -4,22 +4,7 @@ from kafka import KafkaConsumer, KafkaProducer
 import json
 
 def index(request):
-    LAT_LON_KAFKA_TOPIC = "self_lat_lon"
-    consumer = KafkaConsumer(
-        LAT_LON_KAFKA_TOPIC, 
-        bootstrap_servers="103.150.136.82:9092"
-    )
-    consumed_message = []
-    for m in consumer:
-        c = json.loads(m.value.decode())
-        consumed_message.append(c)
-        break
-    da = consumed_message[0]
-    context={
-        'latitude':da['latitude'],
-        'longitude':da['longitude']
-    }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html')
 
 def delivery_boy(request):
     return render(request, 'delivery_boy.html')
@@ -46,6 +31,16 @@ def produce_lat_lon(request):
 
 
 def data(request):
+    c = get_latitude_longitude()
+    da = c[0]
+    return JsonResponse({
+        'latitude':da['latitude'],
+        'longitude':da['longitude']
+    })
+    
+
+def get_latitude_longitude():
+    
     LAT_LON_KAFKA_TOPIC = "self_lat_lon"
     consumer = KafkaConsumer(
         LAT_LON_KAFKA_TOPIC, 
@@ -56,13 +51,5 @@ def data(request):
         c = json.loads(m.value.decode())
         consumed_message.append(c)
         break
-    da = consumed_message[0]
-    return JsonResponse({
-        'latitude':da['latitude'],
-        'longitude':da['longitude']
-    })
-    
-
-
-    
+    return consumed_message
 
