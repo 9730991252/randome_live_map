2ceb082547d5fabaa812016a24b06897d71ede05
9730991252 <sunilkale101@gmail.com>
2024-11-25 17:51:25 +0530

    ok

--- home/views.py
+++ home/views.py
@@ -16,7 +16,7 @@ def produce_lat_lon(request):
         
         LAT_LON_KAFKA_TOPIC = "self_lat_lon"
         
-        producer = KafkaProducer(bootstrap_servers="103.150.136.82:9092")
+        producer = KafkaProducer(bootstrap_servers="localhost:9092")
         
         data = {
             "latitude": lat,
@@ -26,29 +26,27 @@ def produce_lat_lon(request):
                 LAT_LON_KAFKA_TOPIC,
                 json.dumps(data).encode("utf-8")
             )
+        producer.flush()
     return JsonResponse({'latitude': 'status'})
 
 
 def data(request):
-    c = get_latitude_longitude()
-    da = c[0]
-    return JsonResponse({
-        'latitude':da['latitude'],
-        'longitude':da['longitude']
-    })
+    if request.method == 'GET':
+        LAT_LON_KAFKA_TOPIC = "self_lat_lon"
+        consumer = KafkaConsumer(
+            LAT_LON_KAFKA_TOPIC, 
+            bootstrap_servers="localhost:9092"
+        )
+        consumed_message = []
+        for m in consumer:
+            c = json.loads(m.value.decode())
+            consumed_message.append(c)
+            break
+        d = consumed_message[0]
+    return JsonResponse({'latitude': d['latitude'],'longitude':d['longitude']})
     
 
 def get_latitude_longitude():
-    
-    LAT_LON_KAFKA_TOPIC = "self_lat_lon"
-    consumer = KafkaConsumer(
-        LAT_LON_KAFKA_TOPIC, 
-        bootstrap_servers="103.150.136.82:9092"
-    )
-    consumed_message = []
-    for m in consumer:
-        c = json.loads(m.value.decode())
-        consumed_message.append(c)
-        break
-    return consumed_message
+
+    return 'consumed_message'
 
